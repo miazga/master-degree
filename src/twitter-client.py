d08645be06ed975cacf4b3fbd07043fa8bff4c61
Marcin Miazga <miazgamarcin@outlook.com>
2020-07-08 11:33:12 +0200

    update client

--- src/twitter-client.py
+++ src/twitter-client.py
@@ -88,7 +88,7 @@ def main():
     # creating object of TwitterClient Class 
     api = TwitterClient() 
     # calling function to get tweets 
-    tweets = api.get_tweets(query = 'Donald Trump', count = 200) 
+    tweets = api.get_tweets(query = '2FA', count = 200) 
 
     # picking positive tweets from tweets 
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive'] 
